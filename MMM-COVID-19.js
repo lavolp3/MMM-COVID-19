c492b3f7ca2efc3814806a287d53ff0ce08471b7
lavolp3 <dirk.kovert@gmail.com>
2020-03-03 13:38:52 +0100

    partially finished, chart not yet ready, data source ok

--- MMM-COVID-19.js
+++ MMM-COVID-19.js
@@ -6,11 +6,16 @@ Module.register("MMM-COVID-19",{
     rotateInterval: 1000 * 5,
     detailProvince: false,
     pinned: ["Others", "Diamond Princess cruise ship"],
-    sortOrder: null
+    sortOrder: null,
+    dailyGraph: true
   },
 
   getStyles: function() {
     return ["MMM-COVID-19.css"]
+  },
+  
+  getScripts: function() {
+    return [this.file('node_modules/chart.js/dist/Chart.bundle.js')]
   },
 
   start: function() {
@@ -39,6 +44,7 @@ Module.register("MMM-COVID-19",{
       this.drawPinned(dom)
       this.drawEach(dom)
     }
+	if (this.config.dailyGraph) { this.drawChart(dom) }
     return dom
   },
 
@@ -141,6 +147,14 @@ Module.register("MMM-COVID-19",{
     this.drawRegion(parent, t, "rotate")
   },
 
+
+  drawChart: function(parent) {
+    var canvas = document.createElement("canvas")
+    canvas.id = "chart";
+	parent.appendChild(canvas);
+  },
+  
+
   notificationReceived: function(noti, payload, sender) {
 
   },
@@ -155,24 +169,28 @@ Module.register("MMM-COVID-19",{
 
   scanPolling: function() {
     clearTimeout(this.scanTimer)
-    this.scan()
+    this.scan("latest");
+    if (this.config.dailyGraph) {
+      this.scan("timeseries")
+    }
     this.scanTimer = setTimeout(()=>{
       this.scanPolling()
     }, this.config.scanInterval)
   },
 
-  scan: function() {
-    var xhttp = new XMLHttpRequest()
+  scan: function(method) {
+	var self = this;
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/" + method, true)
     xhttp.onreadystatechange = () => {
       if (xhttp.readyState == 4 && xhttp.status == 200) {
-        this.parse(xhttp.responseText)
+        this.parse(xhttp.responseText, method);
       }
     }
-    xhttp.open("GET", "https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest")
     xhttp.send()
   },
 
-  parse: function(response) {
+  parse: function(response, method) {
     var data = null
     try {
       data = JSON.parse(response)
@@ -184,25 +202,140 @@ Module.register("MMM-COVID-19",{
       console.log("[COVID] ERROR: Invalid data format")
       return
     }
-    data = data.sort(this.sortOrder)
-    var indexPool = []
-    if (this.config.detailProvince) {
-      indexPool = data.map((r)=>{
-        return [r.countryregion, r.provincestate]
-      })
-    } else {
-      indexPool = Array.from(new Set(data.map((r)=>{
-        return r.countryregion
-      })))
-    }
-    this.indexPool = indexPool
-    console.log(this.indexPool)
-    this.rawData = data
-    this.store = {}
-    if (this.config.logProvinceCountry && !this.alreadyLogged) {
-      if (this.config.logOnce) this.alreadyLogged = true
-      this.sendSocketNotification("LOG_PRSTCORE", data)
-    }
-    this.drawPolling()
+    if (method === "latest") {
+      data = data.sort(this.sortOrder)
+      var indexPool = []
+      if (this.config.detailProvince) {
+        indexPool = data.map((r)=>{
+          return [r.countryregion, r.provincestate]
+        })
+      } else {
+        indexPool = Array.from(new Set(data.map((r)=>{
+          return r.countryregion
+        })))
+      }
+      this.indexPool = indexPool
+      console.log(this.indexPool)
+      this.rawData = data
+        this.store = {}
+      if (this.config.logProvinceCountry && !this.alreadyLogged) {
+        if (this.config.logOnce) this.alreadyLogged = true
+        this.sendSocketNotification("LOG_PRSTCORE", data)
+      }
+	  this.drawPolling();
+    } else if (method === "timeseries") {
+      //console.log(data);
+      var startDate = moment("2020-01-22");
+      var days = moment().diff(startDate, "days")+1;
+      //console.log(days, data.length);
+      var seriesData = {};
+      for (var d = 0; d < days; d++) {
+        seriesData[startDate.clone().add(d, "days").format("M/D/YY")] = {
+          confirmed: 0,
+          recovered: 0,
+          deaths: 0,
+        }
+      }
+      for (var p = 0; p < data.length; p++) {
+        for (var d in data[p].timeseries) {
+          for (var c in data[p].timeseries[d]) {
+            seriesData[d][c] += data[p].timeseries[d][c]
+          }
+        }
+      }
+      var chartData = [[],[],[]];
+	  var times = [];
+      for (day in seriesData) {
+        var lastDay = moment(day, "M/D/YY").clone().subtract(1, "days").format("M/D/YY");
+		times.push(moment(day, "M/D/YY").format("X"));
+        chartData[0].push(Math.max((seriesData.hasOwnProperty(lastDay)) ? seriesData[day].confirmed - seriesData[lastDay].confirmed : 0, 0));
+        chartData[1].push(Math.max((seriesData.hasOwnProperty(lastDay)) ? seriesData[day].deaths - seriesData[lastDay].deaths : 0, 0));
+		chartData[2].push(Math.max((seriesData.hasOwnProperty(lastDay)) ? seriesData[day].recovered - seriesData[lastDay].recovered : 0, 0));
+      }
+      console.log(times);
+	  console.log(chartData);
+	  this.drawPolling();
+      this.fillChart(chartData, times);
+	}
+  },
+  
+  fillChart: function(chartData, times) {
+    var chart = document.getElementById("chart");
+    chart.style.display = "block";
+    chart.width = 400;
+    chart.height = 300;
+    var ctx = chart.getContext("2d");
+    Chart.defaults.global.defaultFontSize = 14;
+    var coronaChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: times,
+        datasets: [
+          {
+            label: "confirmed",
+            data: chartData[0],
+            barPercentage: 1,
+            categoryPercentage: 0.8,  
+            backgroundColor: 'white',
+            borderWidth: 1,
+            pointRadius: 0,
+            fill: 'origin'
+          },
+          {
+            label: "deaths",
+            data: chartData[1],
+            barPercentage: 1,
+            categoryPercentage: 0.8,  
+            backgroundColor: 'red',
+            borderWidth: 1,
+            pointRadius: 0,
+            fill: 'origin'
+          },
+          {
+            label: "recovered",
+            data: chartData[2],
+            barPercentage: 1,
+            categoryPercentage: 0.8,  
+            backgroundColor: 'green',
+            borderWidth: 1,
+            pointRadius: 0,
+            fill: 'origin'
+          }
+        ],
+      },
+      options: {
+        responsive: false,
+        maintainAspectRatio: true,
+        animation: {
+          duration: 0,
+        },
+        scales: {
+          yAxes: [{
+            display: true,
+            ticks: {
+              suggestedMax: 0.6,
+              display: false,
+            }
+          }],
+          xAxes: [{
+            //type: "time",
+            /*time: {
+              unit: 'day',
+              //parser: "HH:mm",
+              displayFormats: {
+                day: 'MM/DD'
+              },
+            },*/
+            ticks: {
+              fontColor: '#eee',
+              fontSize: 20,
+            }
+          }]
+        },
+        legend: { display: false, },
+        borderColor: 'white',
+        borderWidth: 1,
+      }
+    });
   }
 })
